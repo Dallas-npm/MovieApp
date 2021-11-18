927a0ddc8d8e489d4a2a199897c53cc921672314
Dallas-npm <irnis062@gmail.com>
2021-11-18 23:57:31 +0100

    update

--- src/store/index.js
+++ src/store/index.js
@@ -13,12 +13,6 @@ export const allReducers = combineReducers({
   token: tokenReducer,
 });
 
-const store = createStore(
-  allReducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(allReducers, compose(applyMiddleware(thunk)));
 
 export default store;
